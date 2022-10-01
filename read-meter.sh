7f9dd3bb895ef75488180ba9164e8da226b09cfe
mmdbeer <m.m.d.beer@gmail.com>
2022-10-01 14:54:25 +0200

    add shell script to run reading.py as cron job

---
+++ read-meter.sh
@@ -0,0 +1,6 @@
+source /home/beerm/Documents/python/sm-env/bin/activate
+echo "Python environment activated"
+
+cd /home/beerm/Documents/python/smartmeter
+echo "Current working directory: $PWD" 
+python src/reading.py
