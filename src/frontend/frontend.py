2891505cc157e1982278153441910b21a189e30a
mmdbeer <m.m.d.beer@gmail.com>
2022-10-03 10:10:02 +0200

    first working version frontend

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,2 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
 
+st.title('The amazing smartmeter app')
+
+server_url = "http://0.0.0.0:8432"
+req = requests.get(server_url)
+st.markdown(req.text)
